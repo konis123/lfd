9bfd671cde3e5863b817cba63d21c7dc139fb852
leejinho <konis123@naver.com>
2017-10-10 13:42:04 +0900

    objpoints, imgpoints 저장하는거 나눔

--- test.py
+++ test.py
@@ -86,7 +86,7 @@ for imagePath in IMAGE_PATHS:
 		2.0, (0, 255, 0), 3)
 	cv2.imshow("image", image)
 	cv2.waitKey(0)
-	
+
 '''
 
 
@@ -99,11 +99,11 @@ import glob
 import pickle
 #%matplotlib inline
 
-
-images = glob.glob('./calibration_wide/GOPR00*.jpg')
-
 objpoints = []
 imgpoints = []
+'''
+ #여기는 캘리브레이션 할때만 하면되고 보통 저장한값 불러쓰면댐
+images = glob.glob('./calibration_wide/GOPR00*.jpg')
 
 objp = np.zeros((6*8,3), np.float32)
 objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
@@ -112,25 +112,38 @@ for fname in images:
     img = mpimg.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+    #ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
 
     if ret == True:
         imgpoints.append(corners)
         objpoints.append(objp)
 
 
-with open('calib.p','wb') as f:
+with open('calib_objpoints.p','wb') as f:
     pickle.dump(objpoints, f)
-    pickle.dump(imgpoints, f)
     f.close()
 
+with open('calib_imgpoints.p','wb') as f:
+    pickle.dump(imgpoints, f)
+    f.close()
+'''
 
-temp = cv2.imread('./calibration_wide/GOPR0032.jpg')
+#objpoints하고 imgpoints 가져오기
+with open( "calib_objpoints.p", "rb" ) as f:
+    objpoints =pickle.load(f)
+    f.close()
+with open( "calib_imgpoints.p", "rb" ) as f:
+    imgpoints =pickle.load(f)
+    f.close()
 
+img = cv2.imread('./calibration_wide/GOPR0032.jpg')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
 img = cv2.drawChessboardCorners(img, (8,6), corners, ret)
 #plt.imshow(img)
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-dst = cv2.undistort(temp, mtx, dist, None, mtx)
+dst = cv2.undistort(img, mtx, dist, None, mtx)
 cv2.imshow('asdf', dst)
 key = cv2.waitKey(0)
 #if key==27:
